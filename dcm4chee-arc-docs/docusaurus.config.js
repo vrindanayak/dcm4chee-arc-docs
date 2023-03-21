d902c19500347a34d065a445038e8142f19e41e7
Vrinda <vrinda.nayak@j4care.com>
2023-03-21 15:34:36 +0100

    first commit test dcm4chee-arc-docs

--- dcm4chee-arc-docs/docusaurus.config.js
+++ dcm4chee-arc-docs/docusaurus.config.js
@@ -11,16 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://vrindanayak.github.io',
+  url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dcm4chee-arc-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'vrindanayak', // Usually your GitHub org/user name.
-  projectName: 'dcm4chee-arc-docs', // Usually your repo name.
-  deploymentBranch: "gh-pages",
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/vrindanayak/dcm4chee-arc-docs/tree/master/',
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/vrindanayak/dcm4chee-arc-docs/tree/master/',
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
